e4960ecb7b0545a10ea5cc40b2efdb311e7c4c1c
wxu <winnielandau@gmail.com>
2015-02-09 13:24:16 -0800

    discussion and comments all work properly now, except UI

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,34 @@
 class CommentsController < ApplicationController
-	before_action :find_project
-	before_action :find_comment, only [ :destroy]
+	before_action :find_discussion
+	before_action :find_comment, only: [ :destroy]
 	before_action :authenticate_user! 
 
 	def create
 		@comment = Comment.new comment_params
-		@comment.project = @project
+		@comment.discussion = @discussion
+
 		if @comment.save
-			redirect_to discussion_comments_path, notice: "Comment created successfully"
+			redirect_to project_path(@discussion.project), notice: "Comment created successfully"
 		else
-			redirect_to discussion_comments_path, alert: "err: Comment didn't save!"
+			redirect_to project_path(@discussion.project), alert: "err: Comment didn't save!"
 		end
 	end
 
 	def destroy
 		@comment.destroy
-		redirect_to discussion_comments_path, notice: "Comment deleted successfully"
+		respond_to do |format|
+			format.js { render } #render "/comments/destroy.js.erb"
+			format.html { redirect_to project_path(@discussion.project), notice: "Comment deleted successfully" }
+			#redirect_to project_path(@discussion.project), notice: "Comment deleted successfully"
+
+		end
+
 	end
 
 
 	private
-	def find_project
-		@project = Project.find params[:project_id]
+	def find_discussion
+		@discussion = Discussion.find params[:discussion_id]
 	end
 	def find_comment
 		@comment = Comment.find(params[:id])
