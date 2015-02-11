82f9936814374ee52ae3410e28e2b6ac89b3d686
wxu <winnielandau@gmail.com>
2015-02-11 15:58:10 -0800

    start on AJAX for comment and task

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,19 +1,36 @@
 class CommentsController < ApplicationController
 	before_action :find_discussion
-	before_action :find_comment, only: [ :destroy]
+	before_action :find_comment, only: [:update, :destroy]
 	before_action :authenticate_user! 
 
 	def create
 		@comment = Comment.new comment_params
 		@comment.discussion = @discussion
 
-		if @comment.save
-			# delayed email job
-      DiscussionMailer.notify_discussion_owner(@comment).deliver_later
+		respond_to do |format|		
+			if @comment.save
+				# delayed email job
+	      DiscussionMailer.notify_discussion_owner(@comment).deliver_later				
+				format.js { render } #render /comments/create.js.erb
+				format.html { 			redirect_to project_path(@discussion.project), notice: "Comment created successfully" }
+			else
+				format.js { render } #render /comments/create.js.erb
+				format.html { 			redirect_to project_path(@discussion.project), alert: "err: Comment didn't save!" }
+			end
+		end
+	end
 
-			redirect_to project_path(@discussion.project), notice: "Comment created successfully"
-		else
-			redirect_to project_path(@discussion.project), alert: "err: Comment didn't save!"
+	def update
+		#find_project
+		#find_comment
+		respond_to do |format|
+			if @comment.update comment.params
+				format.js { render }
+				format.html { redirect_to project_path(@project), notice: "Comment updated successfully"}
+			else
+				format.js { render }
+				format.html { render :edit, alert: "Comment failed to update"}
+			end
 		end
 	end
 
@@ -22,8 +39,6 @@ class CommentsController < ApplicationController
 		respond_to do |format|
 			format.js { render } #render "/comments/destroy.js.erb"
 			format.html { redirect_to project_path(@discussion.project), notice: "Comment deleted successfully" }
-			#redirect_to project_path(@discussion.project), notice: "Comment deleted successfully"
-
 		end
 
 	end
--- lib/tasks/pm_tool.rake
+++ lib/tasks/pm_tool.rake
@@ -5,17 +5,6 @@ namespace :pm_tool do
 
   end
 
-
-  desc "Generate ten questions with 10 answers each"
-  task :questions_and_answers => :environment do
-    10.times do
-      question = Question.create(title: Faker::Lorem.sentence(10), description: Faker::Lorem.sentence(30))
-      10.times do
-        question.answers.create(body: Faker::Company.bs)
-      end
-    end
-  end
-
   desc "Generate a summary list of all newly created discussions that day for projects"
   task :list_discussions => :environment do
 
